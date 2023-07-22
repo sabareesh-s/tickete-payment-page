248a2461a4b0306937a64e4155942294f2fbeae7
sabareesh-s <sabareesh1221@gmail.com>
2023-07-22 13:02:49 +0530

    added header component

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,27 @@
+import TicketeLogo from "@/icons/TicketeLogo";
+import HelpIcon from "@/icons/helpIcon";
+import React from "react";
+
+interface HeaderProps {
+  title: string;
+  onClick?: () => void;
+}
+
+const Header: React.FC<HeaderProps> = ({ title, onClick }) => {
+  return (
+    <header className="w-full bg-blue-200">
+      <h1 className="h-[4.5rem] w-[80rem] flex justify-between px-10 items-center mx-auto">
+        <a href='#' className="my-auto">
+          <TicketeLogo/>
+        </a>
+        
+        {onClick && <button className="text-gray2 w-[5.75rem] transition-colors h-11 flex items-center justify-center space-x-1 rounded-md hover:bg-slate-200" onClick={onClick}>
+          <HelpIcon/>
+          <span>Help</span>
+          </button>}
+      </h1>
+    </header>
+  );
+};
+
+export default Header;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+'use client';
 import Image from 'next/image'
+import Header from './components/Header'
 
 export default function Home() {
   return (
     <main className="">
-      tickete
+      <Header title="tickete"  onClick={() => console.log("Help button clicked")} />
     </main>
   )
 }
